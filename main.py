4a885d5643392c5fa5275dc181bc15079ee00148
Matana04 <mateusmatana12@gmail.com>
2023-05-30 22:41:53 -0300

    Correção de erros.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import pandas as pd
 class Ui(QtWidgets.QDialog):
     def __init__(self):
         super(Ui, self).__init__()
-        uic.loadUi('C:/Users/mateu/Desktop/ApiCovid19/tela_api.ui', self)
+        uic.loadUi('C:/Users/mateu/apicovid19/tela_api.ui', self)
         self.show()
 
         vetor = ["ac", "al", "ap", "am", "ba", "ce", "es", "go", "ma", "mt", "ms", "mg", "pa", "pb", "pr", "pe", "pi", "rj", "rn", "ro", "rs", "rr", "sc", "sp", "se", "to", "df"]
@@ -17,6 +17,36 @@ class Ui(QtWidgets.QDialog):
         self.consultaButton.clicked.connect(self.opc)
         self.consultaButton_2.clicked.connect(self.pegatxt)
 
+    def pegatxt(self):
+        inputtxt = self.textEdit.toPlainText()
+        self.textBrowser_2.setText(inputtxt)
+
+        dadoscovid19 = requests.get(f'https://covid19-brazil-api.now.sh/api/report/v1/brazil/{inputtxt}')
+        dicCovid = json.loads(dadoscovid19.content)
+
+        if dicCovid != {"data": []}:
+            dicCovid2 = dicCovid['data']
+
+            Estados = []
+            Casos = []
+            Espaco = []
+
+            for state in dicCovid2:
+                listEstados = state
+                listEstados2 = listEstados.values()
+                listEstados2 = list(listEstados2)
+                Estados.append(listEstados2[2])
+                Casos.append(listEstados2[3])
+                Espaco.append(" --- ")
+
+                tabela = pd.DataFrame(columns=["Estados ", " --- ", " Casos"],
+                                data=zip(Estados, Espaco, Casos))
+
+                tabela2 = str(tabela)
+                self.textBrowser_2.setText(tabela2)
+        else:
+            self.textBrowser_2.setText("A API não possui resultado para essa data, digite outra de acordo com o exemplo: 20201224.")
+
     def opc(self):
         inputUF = self.comboBox.currentText()
 
@@ -24,7 +54,6 @@ class Ui(QtWidgets.QDialog):
         print(reqURL)
 
         response = requests.get(reqURL)
-
         contentJson = json.loads(response.content)
 
 
@@ -53,30 +82,7 @@ class Ui(QtWidgets.QDialog):
         else:
             self.textBrowser.setText("UF não reconhecida.")
 
-    def pegatxt(self):
-        inputtxt = self.textEdit.toPlainText()
 
-        dadoscovid19 = requests.get(f'https://covid19-brazil-api.now.sh/api/report/v1/brazil/{inputtxt}')
-        dicCovid = json.loads(dadoscovid19.content)
-
-        dicCovid2 = dicCovid['data']
-
-        Estados = []
-        Casos = []
-        Mortes = []
-
-        for state in dicCovid2:
-            listEstados = state
-            listEstados2 = listEstados.values()
-            listEstados2 = list(listEstados2)
-            Estados.append(listEstados2[2])
-            Casos.append(listEstados2[3])
-            Mortes.append(listEstados2[4])
-
-        tabela = pd.DataFrame(columns=["Estados", "Casos", "Mortes"],
-                              data=zip(Estados, Casos, Mortes))
-
-        self.textBrowser_2.setText(tabela.__str__())
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
